49ff145477665d5f7ab1db0b2227ed8502b96a25
Jason Butwell <jasonbutwell@gmail.com>
2016-12-01 21:05:22 +0000

    Created test interface to capture information to send in the email.

--- app/src/main/java/com/jasonbutwell/emailintentdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/emailintentdemo/MainActivity.java
@@ -1,13 +1,41 @@
 package com.jasonbutwell.emailintentdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText phone;
+    EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText)findViewById(R.id.Name);
+        phone = (EditText)findViewById(R.id.PhoneNo);
+        message = (EditText)findViewById(R.id.Message);
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
