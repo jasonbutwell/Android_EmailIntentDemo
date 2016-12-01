f78fb356c77e2c4876fffa1e6cd23f0403ef398c
Jason Butwell <jasonbutwell@gmail.com>
2016-12-01 21:54:04 +0000

    Added additional field for the subject line and implemented the method sendEmail to pull text from the fields to construct the email to send.

--- app/src/main/java/com/jasonbutwell/emailintentdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/emailintentdemo/MainActivity.java
@@ -1,16 +1,18 @@
 package com.jasonbutwell.emailintentdemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText name;
-    EditText phone;
-    EditText message;
+    private EditText name, phone, subject, message;
+
+    Intent emailIntent, chooserIntent = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +21,39 @@ public class MainActivity extends AppCompatActivity {
 
         name = (EditText)findViewById(R.id.Name);
         phone = (EditText)findViewById(R.id.PhoneNo);
+        subject = (EditText)findViewById(R.id.subject);
         message = (EditText)findViewById(R.id.Message);
+    }
 
-        name.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    public void sendText( View view ) {
+        
+    }
 
-            }
+    public void sendEmail(View view) {
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        boolean successful = true;
 
-            }
+        if ( name.length() < 1 || phone.length() < 1 || subject.length() < 1 || message.length() < 1 )
+            successful = false;
 
-            @Override
-            public void afterTextChanged(Editable editable) {
+        if ( !successful )
+            Toast.makeText(getApplicationContext(),"Please fill out all required fields",Toast.LENGTH_LONG).show();
+        else {
+            emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.setData(Uri.parse("mailto:"));
 
-            }
-        });
+            String[] to = {"jasonbutwell@gmail.com"};
+
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+
+            String bodyTextEmail = "Email from: " + name.getText().toString() + ", Phone number: " + phone.getText().toString() + "\n\n"+ message.getText().toString();
+
+            emailIntent.putExtra(Intent.EXTRA_TEXT, bodyTextEmail);
+            emailIntent.setType("message/rfc822");
+
+            chooserIntent = Intent.createChooser(emailIntent, "Send Email");
+            startActivity(chooserIntent);
+        }
     }
 }
