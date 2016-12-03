187e2fe4e90fe891b7da34e0e4caf624eefc7a0f
Jason Butwell <jasonbutwell@gmail.com>
2016-12-03 02:04:32 +0000

    Started adding email attachment functionality. Not fully working yet.

--- app/src/main/java/com/jasonbutwell/emailintentdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/emailintentdemo/MainActivity.java
@@ -1,14 +1,23 @@
 package com.jasonbutwell.emailintentdemo;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+    int REQUEST_CODE_LOAD_IMAGE = 101;
+    String picturePath;
+    Uri imagepath;
 
     private EditText name, phone, subject, message;
 
@@ -23,6 +32,38 @@ public class MainActivity extends AppCompatActivity {
         phone = (EditText)findViewById(R.id.PhoneNo);
         subject = (EditText)findViewById(R.id.subject);
         message = (EditText)findViewById(R.id.Message);
+    }
+
+    public void openGallery(View view) {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra("return-data",true);
+        startActivityForResult(Intent.createChooser(intent,"Compete Action Using"),REQUEST_CODE_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            imagepath = selectedImage;
+
+            Log.i("file",selectedImage.toString());
+
+        }
     }
 
     public void openWebPage(View view) {
@@ -68,8 +109,17 @@ public class MainActivity extends AppCompatActivity {
             emailIntent.putExtra(Intent.EXTRA_TEXT, bodyTextEmail);
             emailIntent.setType("message/rfc822");
 
+            if ( imagepath != null ) {
+                File file = new File(this.getFilesDir().getAbsolutePath()+picturePath);
+                emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                emailIntent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+                emailIntent.setType("image/jpeg");
+                emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://"+file.getAbsolutePath())); //Your image file
+            }
+
             chooserIntent = Intent.createChooser(emailIntent, "Send Email");
             startActivity(chooserIntent);
         }
     }
+
 }
